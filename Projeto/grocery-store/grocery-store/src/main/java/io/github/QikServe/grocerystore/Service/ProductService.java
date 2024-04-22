05e1f6d4d3d4aea20dbda8ca40abd746def4532f
Jasson Luiz <jassonluizjr@hotmail.com>
2024-04-22 16:21:47 -0300

    Adds comments to Service methods

--- Projeto/grocery-store/grocery-store/src/main/java/io/github/QikServe/grocerystore/Service/ProductService.java
+++ Projeto/grocery-store/grocery-store/src/main/java/io/github/QikServe/grocerystore/Service/ProductService.java
@@ -6,6 +6,7 @@ import io.github.QikServe.grocerystore.DTO.PromotionDTO;
 import io.github.QikServe.grocerystore.Exception.ResourceNotFoundException;
 import io.github.QikServe.grocerystore.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -18,112 +19,169 @@ import java.util.List;
 @Service
 public class ProductService {
 
-        private static final String WIREMOCKURL = "http://localhost:8081";
+    @Value("${wiremock.url}")
+    private String wireMockUrl;
 
-        private RestTemplate restTemplate;
+    private RestTemplate restTemplate;
 
-        @Autowired
-        public ProductService(RestTemplate restTemplate){
-            this.restTemplate = restTemplate;
+    @Autowired
+    public ProductService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    /**
+     * Executes a GET or POST request using the RestTemplate to obtain an object of the specified type.
+     * @param url The URL to which the request will be sent.
+     * @param responseType The class that represents the expected type of the response.
+     * @return The type of the response object.
+     * @param <T> The response object obtained after the request.
+     */
+    private <T> T executeRestTemplate(String url, Class<T> responseType) {
+        return restTemplate.getForObject(url, responseType);
+    }
+
+
+    /**
+     * Get all service products.
+     *
+     * @return An array of products obtained from the service.
+     */
+    public Product[] getAllProducts() {
+        Product[] products;
+        try {
+            products = executeRestTemplate(wireMockUrl + "/products", Product[].class);
+        } catch (HttpClientErrorException.NotFound e) {
+            products = new Product[0];
         }
+        return products;
+    }
 
-        public Product[] getAllProducts(){
-            Product[] products;
-            try {
-                products = restTemplate.getForObject(WIREMOCKURL + "/products", Product[].class);
-            }catch (HttpClientErrorException.NotFound e){
-                products = new Product[0];
-            }
-            return products;
+    /**
+     * Gets a single product by its service ID.
+     * @param id The ID of the product to get.
+     * @return The product corresponding to the specified ID.
+     */
+    public Product getOneProduct(String id) {
+        Product product;
+        try {
+            product = executeRestTemplate(wireMockUrl + "/products/" + id, Product.class);
+        } catch (HttpClientErrorException.NotFound e) {
+            product = null;
         }
+        return product;
+    }
 
-        public Product getOneProduct(String id){
-            Product product;
-            try {
-                product = restTemplate.getForObject(WIREMOCKURL + "/products/" + id, Product.class);
-            }catch (HttpClientErrorException.NotFound e){
-                product = null;
-            }
-            return product;
-        }
+    /**
+     * Add items to your shopping cart and calculate the total value and savings for each item.
+     * @param items A list of items to add to the shopping cart.
+     * @return A list of ItemCheckoutDTO objects containing details of each item added to the cart.
+     */
+    public List<ItemCheckoutDTO> addToCart(List<CartItemDTO> items) {
+        List<ItemCheckoutDTO> itemCheckouts = new ArrayList<>();
 
-        public List<ItemCheckoutDTO> addToCart(List<CartItemDTO> items){
-            List<ItemCheckoutDTO> itemCheckouts = new ArrayList<>();
-
-            for (CartItemDTO item : items) {
-                    Product product = restTemplate.getForObject(WIREMOCKURL + "/products/" + item.getProductId(), Product.class);
-                    if(product == null){
-                        throw  new ResourceNotFoundException("Product is not found for this ID!");
-                    }
-
-                    BigDecimal quantity = new BigDecimal(item.getQuantity());
-
-                    BigDecimal productValue = new BigDecimal(product.getPrice());
-                    BigDecimal itemTotal = productValue.multiply(quantity);
-                    BigDecimal itemSavings = BigDecimal.ZERO;
-
-                    for (PromotionDTO promotion : product.getPromotions()){
-                        switch (promotion.getType()){
-                            case FLAT_PERCENT:
-                                BigDecimal[] resultFlatPercent = applyFlatPercentDiscount(itemTotal, productValue, quantity, promotion.getAmount(), itemSavings);
-                                itemTotal = resultFlatPercent[0];
-                                itemSavings = resultFlatPercent[1];
-                                break;
-                            case BUY_X_GET_Y_FREE:
-                                BigDecimal[] resultBuyXGetYFree = applyBuyXGetYFreeDiscount(itemTotal, productValue, quantity, promotion.getRequiredQty(), itemSavings);
-                                itemTotal = resultBuyXGetYFree[0];
-                                itemSavings = resultBuyXGetYFree[1];
-                                break;
-                            case QTY_BASED_PRICE_OVERRIDE:
-                                BigDecimal[] resultQtyBasedPriceOverride = applyQtyBasedPriceOverrideDiscount(itemTotal, productValue, quantity, promotion.getRequiredQty(), promotion.getPrice(), itemSavings);
-                                itemTotal = resultQtyBasedPriceOverride[0];
-                                itemSavings = resultQtyBasedPriceOverride[1];
-                                break;
-                            default:
-                                throw new ResourceNotFoundException("Promotion type does not exist");
-                        }
-                    }
-
-                    ItemCheckoutDTO dto = new ItemCheckoutDTO(product, quantity, itemTotal, itemSavings);
-                    itemCheckouts.add(dto);
+        for (CartItemDTO item : items) {
+            Product product = executeRestTemplate(wireMockUrl + "/products/" + item.getProductId(), Product.class);
+            if (product == null) {
+                throw new ResourceNotFoundException("Product is not found for this ID!");
             }
 
-            return itemCheckouts;
-        }
+            BigDecimal quantity = new BigDecimal(item.getQuantity());
 
-        private BigDecimal[] applyFlatPercentDiscount(BigDecimal itemTotal, BigDecimal productValue, BigDecimal quantity, double discountPercent, BigDecimal itemSavings){
-            BigDecimal discountAmount = productValue.multiply(BigDecimal.valueOf(discountPercent)).divide(BigDecimal.valueOf(100));
-            BigDecimal totalDiscount = discountAmount.multiply(quantity);
+            BigDecimal productValue = new BigDecimal(product.getPrice());
+            BigDecimal itemTotal = productValue.multiply(quantity);
+            BigDecimal itemSavings = BigDecimal.ZERO;
 
-            itemTotal = itemTotal.subtract(totalDiscount);
-            itemSavings = itemSavings.add(totalDiscount);
-
-            return new BigDecimal[]{itemTotal, itemSavings};
-        }
-
-        private BigDecimal[] applyBuyXGetYFreeDiscount(BigDecimal itemTotal, BigDecimal productValue, BigDecimal quantity, int requiredQty, BigDecimal itemSavings){
-            BigDecimal qtFree = quantity.divide(new BigDecimal(requiredQty), 0, RoundingMode.DOWN);
-            BigDecimal totalFreeItemsValue = productValue.multiply(qtFree);
-
-            itemTotal = itemTotal.subtract(totalFreeItemsValue);
-            itemSavings = itemSavings.add(totalFreeItemsValue);
-
-            return new BigDecimal[]{itemTotal, itemSavings};
-        }
-
-        private BigDecimal[] applyQtyBasedPriceOverrideDiscount(BigDecimal itemTotal, BigDecimal productValue, BigDecimal quantity, int requiredQty, double promoPrice, BigDecimal itemSavings){
-            int qtPromo = quantity.intValue() / requiredQty;
-            int qtNonPromo = quantity.intValue() % requiredQty;
-
-            BigDecimal totalDiscountedPrice  = new BigDecimal(qtPromo).multiply(BigDecimal.valueOf(promoPrice));
-
-            if(qtNonPromo > 0){
-                totalDiscountedPrice  = totalDiscountedPrice .add(productValue);
+            for (PromotionDTO promotion : product.getPromotions()) {
+                switch (promotion.getType()) {
+                    case FLAT_PERCENT:
+                        BigDecimal[] resultFlatPercent = applyFlatPercentDiscount(itemTotal, productValue, quantity, promotion.getAmount(), itemSavings);
+                        itemTotal = resultFlatPercent[0];
+                        itemSavings = resultFlatPercent[1];
+                        break;
+                    case BUY_X_GET_Y_FREE:
+                        BigDecimal[] resultBuyXGetYFree = applyBuyXGetYFreeDiscount(itemTotal, productValue, quantity, promotion.getRequiredQty(), itemSavings);
+                        itemTotal = resultBuyXGetYFree[0];
+                        itemSavings = resultBuyXGetYFree[1];
+                        break;
+                    case QTY_BASED_PRICE_OVERRIDE:
+                        BigDecimal[] resultQtyBasedPriceOverride = applyQtyBasedPriceOverrideDiscount(itemTotal, productValue, quantity, promotion.getRequiredQty(), promotion.getPrice(), itemSavings);
+                        itemTotal = resultQtyBasedPriceOverride[0];
+                        itemSavings = resultQtyBasedPriceOverride[1];
+                        break;
+                    default:
+                        throw new ResourceNotFoundException("Promotion type does not exist");
+                }
             }
 
-            itemSavings = itemSavings.add(itemTotal.subtract(totalDiscountedPrice));
-            itemTotal = itemTotal.subtract(itemSavings);
-
-            return new BigDecimal[]{itemTotal, itemSavings};
+            ItemCheckoutDTO dto = new ItemCheckoutDTO(product, quantity, itemTotal, itemSavings);
+            itemCheckouts.add(dto);
         }
+
+        return itemCheckouts;
+    }
+
+
+    /**
+     * Applies a fixed percentage discount to the total value of the item.
+     * @param itemTotal The total value of the item before discount.
+     * @param productValue The unit value of the product.
+     * @param quantity The quantity of the product.
+     * @param discountPercent The discount percentage to be applied.
+     * @param itemSavings The total savings accumulated to date.
+     * @return An array containing the new total value of the item after the discount and the updated savings total.
+     */
+    private BigDecimal[] applyFlatPercentDiscount(BigDecimal itemTotal, BigDecimal productValue, BigDecimal quantity, double discountPercent, BigDecimal itemSavings) {
+        BigDecimal discountAmount = productValue.multiply(BigDecimal.valueOf(discountPercent)).divide(BigDecimal.valueOf(100));
+        BigDecimal totalDiscount = discountAmount.multiply(quantity);
+
+        itemTotal = itemTotal.subtract(totalDiscount);
+        itemSavings = itemSavings.add(totalDiscount);
+
+        return new BigDecimal[]{itemTotal, itemSavings};
+    }
+
+    /**
+     * Applies a "buy X, get Y free" discount to the total value of the item.
+     * @param itemTotal The total value of the item before discount.
+     * @param productValue The unit value of the product.
+     * @param quantity The quantity of the product.
+     * @param requiredQty The quantity required to activate the discount.
+     * @param itemSavings The total savings accumulated to date.
+     * @return An array containing the new total value of the item after the discount and the updated savings total.
+     */
+    private BigDecimal[] applyBuyXGetYFreeDiscount(BigDecimal itemTotal, BigDecimal productValue, BigDecimal quantity, int requiredQty, BigDecimal itemSavings) {
+        BigDecimal qtFree = quantity.divide(new BigDecimal(requiredQty), 0, RoundingMode.DOWN);
+        BigDecimal totalFreeItemsValue = productValue.multiply(qtFree);
+
+        itemTotal = itemTotal.subtract(totalFreeItemsValue);
+        itemSavings = itemSavings.add(totalFreeItemsValue);
+
+        return new BigDecimal[]{itemTotal, itemSavings};
+    }
+
+    /**
+     *
+     * @param itemTotal The total value of the item before discount.
+     * @param productValue The unit value of the product.
+     * @param quantity The quantity of the product.
+     * @param requiredQty The quantity required to activate the discount.
+     * @param promoPrice  The promotional price to be applied when the minimum quantity is reached.
+     * @param itemSavings The total savings accumulated to date.
+     * @return An array containing the new total value of the item after the discount and the updated savings total.
+     */
+    private BigDecimal[] applyQtyBasedPriceOverrideDiscount(BigDecimal itemTotal, BigDecimal productValue, BigDecimal quantity, int requiredQty, double promoPrice, BigDecimal itemSavings) {
+        int qtPromo = quantity.intValue() / requiredQty;
+        int qtNonPromo = quantity.intValue() % requiredQty;
+
+        BigDecimal totalDiscountedPrice = new BigDecimal(qtPromo).multiply(BigDecimal.valueOf(promoPrice));
+
+        if (qtNonPromo > 0) {
+            totalDiscountedPrice = totalDiscountedPrice.add(productValue);
+        }
+
+        itemSavings = itemSavings.add(itemTotal.subtract(totalDiscountedPrice));
+        itemTotal = itemTotal.subtract(itemSavings);
+
+        return new BigDecimal[]{itemTotal, itemSavings};
+    }
 }
